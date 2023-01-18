9c3b7c9ffbcd6a4997868a15aeb05b90feda90b2
Sylvain Goumy <goumsss@protonmail.com>
2023-01-18 18:31:12 +0100

    chore refactor task creation scripts

---
+++ src/scripts/create-task-event-listener.ts
@@ -0,0 +1,58 @@
+import dotenv from "dotenv";
+import { ethers } from "ethers";
+import { GelatoOpsSDK } from "@gelatonetwork/ops-sdk";
+import { JsResolverBuilder } from "@gelatonetwork/js-resolver-sdk/builder";
+dotenv.config();
+
+if (!process.env.PRIVATE_KEY) throw new Error("Missing env PRIVATE_KEY");
+const pk = process.env.PRIVATE_KEY;
+
+if (!process.env.PROVIDER_URL) throw new Error("Missing env PROVIDER_URL");
+const providerUrl = process.env.PROVIDER_URL;
+
+// Default Setting
+const chainId = 5;
+const counterAddress = "0x8F143A5D62de01EAdAF9ef16d4d3694380066D9F";
+const counterAbi = ["function increaseCount(uint256)"];
+
+const main = async () => {
+  // Instanciate provider & signer
+  const provider = new ethers.providers.JsonRpcProvider(providerUrl);
+  const wallet = new ethers.Wallet(pk as string, provider);
+  const opsSdk = new GelatoOpsSDK(chainId, wallet);
+
+  // Deploy JsResolver on IPFS
+  console.log("Deploying JsResolver on IPFS...");
+  const jsResolver = "./src/resolvers/event-listener/index.ts";
+  const cid = await JsResolverBuilder.deploy(jsResolver);
+  console.log(`JsResolver IPFS CID: ${cid}`);
+
+  // Create task using ops-sdk
+  console.log("Creating automate task...");
+  const counterInterface = new ethers.utils.Interface(counterAbi);
+  const { taskId, tx } = await opsSdk.createTask({
+    name: "JsResolver - Event Counter",
+    execAddress: counterAddress,
+    execSelector: counterInterface.getSighash("increaseCount"),
+    dedicatedMsgSender: true,
+    jsResolverHash: cid,
+    jsResolverArgs: {
+      oracle: "0x6a3c82330164822A8a39C7C0224D20DB35DD030a",
+      counter: "0x8F143A5D62de01EAdAF9ef16d4d3694380066D9F",
+    },
+  });
+  await tx.wait();
+  console.log(`Task created, taskId: ${taskId} (tx hash: ${tx.hash})`);
+  console.log(
+    `> https://beta.app.gelato.network/task/${taskId}?chainId=${chainId}`
+  );
+};
+
+main()
+  .then(() => {
+    process.exit();
+  })
+  .catch((err) => {
+    console.error("Error:", err.message);
+    process.exit(1);
+  });
