b7a988b23dd6a8676917a73f6cfa44cc2c9fc8e8
Sylvain Goumy <goumsss@protonmail.com>
2023-01-26 15:01:17 +0100

    feat: add simple storage example

---
+++ src/web3Functions/storage/index.ts
@@ -0,0 +1,25 @@
+import {
+  Web3Function,
+  Web3FunctionContext,
+} from "@gelatonetwork/web3-functions-sdk";
+
+Web3Function.onRun(async (context: Web3FunctionContext) => {
+  const { storage, provider } = context;
+
+  // Use storage to retrieve previous state (stored values are always string)
+  const lastBlockStr = (await storage.get("lastBlockNumber")) ?? "0";
+  const lastBlock = parseInt(lastBlockStr);
+  console.log(`Last block: ${lastBlock}`);
+
+  const newBlock = await provider.getBlockNumber();
+  console.log(`New block: ${newBlock}`);
+  if (newBlock > lastBlock) {
+    // Update storage to persist your current state (values must be cast to string)
+    await storage.set("lastBlockNumber", newBlock.toString());
+  }
+
+  return {
+    canExec: false,
+    message: `Updated block number: ${newBlock.toString()}`,
+  };
+});
